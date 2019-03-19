c532f3880b83320576ac5de6193da2b88485433b
zhuimenran <2161434564@qq.com>
2019-03-19 19:32:10 +0800

    create maven project

---
+++ src/main/java/com/txb/whut/my_maven_program/App.java
@@ -0,0 +1,13 @@
+package com.txb.whut.my_maven_program;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
